4b3a9ba435fd04e5627e7bd182ecdf100d9d38d5
sorakann iMac <ski@student.42lausanne.com>
2021-12-10 16:03:47 +0100

    amelioration

--- test_externe.c
+++ test_externe.c
@@ -15,16 +15,21 @@
 void	test_externe(void)
 {
 	int		fd;
-	char	*filename = "text/text2";
+	char	*filename = "text/text5";
 	char	buffer[BUFFER_SIZE + 1];
 	ssize_t	n_read_char;
 
+	buffer[BUFFER_SIZE] = '\0';
 	n_read_char = 0;
+
 	/* OUVERTURE DE FICHIER ------------------------------ */
+
 	fd = sub_open_rdonly(filename);
 	if (fd == -1)
 		return ;
+
 	/* LECTURE DE FICHIER -------------------------------- */
+
 	n_read_char = sub_read(fd, buffer, BUFFER_SIZE);
 	if (n_read_char != -1)
 	{
@@ -34,12 +39,17 @@ void	test_externe(void)
 			n_read_char = sub_read(fd, (void*)buffer, BUFFER_SIZE);
 		}	
 	}
+
 	/* FERMETURE FICHIER --------------------------------- */
+
 	sub_close (fd);
+	
 	/* --------------------------------------------------- */
 	return ;
 }
 
+/* ************************************************************************** */
+/* ************************************************************************** */
 /* ************************************************************************** */
 int sub_open_rdonly(char *filename)
 {
@@ -50,27 +60,35 @@ int sub_open_rdonly(char *filename)
 	if (fd == -1)
 		printf("Open file: !!! FAILED !!!\n");		
 	/* --------------------------------------------------- */
-	printf("\n");
-	printf("***********************************\n");
-	printf("Open [%s]: SUCCEED\n", filename);
-	printf("File descriptor: %d | BUFFER_SIZE: %d\n", fd, BUFFER_SIZE);
-	printf("***********************************\n");
-	printf("\n");
+	else
+	{
+		printf("\n");
+		printf("***********************************\n");
+		printf("Open [%s]: SUCCEED\n", filename);
+		printf("File descriptor: %d | BUFFER_SIZE: %d\n", fd, BUFFER_SIZE);
+		printf("***********************************\n");
+		printf("\n");
+	}
 	return (fd);
 }
 /* ************************************************************************** */
-int sub_read(int fd, void *buffer, int buf_size)
+int sub_read(int fd, char *buffer, int buf_size)
 {
 	ssize_t n_read_char;
 
-	n_read_char = read(fd, buffer, buf_size);
+	n_read_char = read(fd, (void *)buffer, buf_size);
 	if (n_read_char == -1)
 	{
 		printf("Read file: !!! FAILED !!!\n");
 		sub_close (fd);
 		return (n_read_char);
 	}
-	printf("Read file: %zd characters read\n", n_read_char);
+	if (n_read_char != 0)
+	{
+		// if (n_read_char != BUFFER_SIZE)
+		// 	buffer[n_read_char - 1] = '\0';
+		printf("Read file: %02zd characters read | [%s]\n", n_read_char, buffer);
+	}
 	if (n_read_char == 0)
 	{
 		printf("\n");
@@ -87,10 +105,12 @@ int sub_close (int fd)
 	int result;
 
 	result = close(fd);
+	printf("**********************************\n");
 	if(result == 0)
 		printf("Close file: Succeed\n");
 	else
 		printf("Close file: !!! FAILED !!!\n");
+	printf("**********************************\n");
 	 return (result);
  }
 /* ************************************************************************** */
--- test_gnl.c
+++ test_gnl.c
@@ -14,5 +14,50 @@
 /* ************************************************************************** */
 void	test_gnl(void)
 {
+	int		fd;
+	char	*filename = "text/text2";
+	char	buffer[BUFFER_SIZE + 1];
+	ssize_t	n_read_char;
+	char	*read_line;
+	int		cnt_line;
+
+
+	read_line = NULL;
+	cnt_line = 0;
+
+	n_read_char = 0;
+	buffer[BUFFER_SIZE] = '\0';
+
+	/* OUVERTURE DE FICHIER ------------------------------ */
+	fd = sub_open_rdonly(filename);
+	if (fd == -1)
+		return ;
+
+	/* GET NEXT LINE ------------------------------------- */
+
+	read_line = get_next_line();
+	cnt_line++;
+	
+	while (read_line != NULL)
+	{
+		printf("buffer: %s\n", buffer);
+		free(read_line);
+
+		read_line = get_next_line();
+		cnt_line++;
+	}
+
+
+	if (read_line == NULL)
+	{
+		printf("*************************************\n");
+		printf("Nothing to read more OR Error occured\n");
+		printf("*************************************\n");
+	}	
+	
+	/* FERMETURE FICHIER --------------------------------- */
+	sub_close (fd);
+
+	/* --------------------------------------------------- */	
 	return ;
 }
